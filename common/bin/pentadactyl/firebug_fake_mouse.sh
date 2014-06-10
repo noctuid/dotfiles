35df823ed15ae9de3c9c5b0b416a7047da444dde
angelic-sedition <gopv1c3@gmail.com>
2014-06-10 01:19:25 -0400

    move

--- common/bin/pentadactyl/firebug_fake_mouse.sh
+++ common/bin/pentadactyl/firebug_fake_mouse.sh
@@ -4,10 +4,10 @@
 xdotool mousemove 519 326 && xdotool click 1 && sleep 3
 
 # to prevent other menu from coming up, return to normal mode
-xdotool key Escape
+xdotool key Escape && sleep 0.2
 # open menu and copy link
 xdotool mousemove 284 693 && xdotool click 3
-sleep 0.2
+sleep 0.4
 xdotool mousemove 376 430 && xdotool click 1
 # stop video playing in broswser
 xdotool mousemove 519 326 && xdotool click 1 && sleep 0.2
@@ -15,4 +15,4 @@ xdotool key Escape
 # open link with mpv fullscreen
 clipboard_contents=$(xsel -b)
 # & so can still use ff while video playing; otherwise must close video first
-mpv --fs $clipboard_contents &
+mpv $clipboard_contents &
