f2fe455606b25bcfe2ec6f65e1bc9a7127fc340a
angelic-sedition <gopv1c3@gmail.com>
2014-07-14 22:03:28 -0400

    update browsing stuff; get tab groups working; add bindings for youtube-dl; use webcomic reader with dta for downloading; fix some regex; start sourcing navigation.penta on location change; other minor changes

--- common/bin/dlf.sh
+++ common/bin/dlf.sh
@@ -6,9 +6,10 @@ site=$1
 url=$2
 
 if [ "$site" == "fakku" ]; then
-	mkdir -p fakku && cd fakku
+	# mkdir -p fakku && cd fakku
 	# fakku.py (renamed without the .py or change below) in path: https://github.com/darkfeline/fakku_downloader
-	fakku $url &
+	# it broke; using comic
+	# fakku $url &
 elif [ "$site" == "gel" ]; then
 	# in aur https://code.google.com/p/danbooru-v7sh-grabber/
 	mkdir -p gelbooru && cd gelbooru
@@ -16,18 +17,28 @@ elif [ "$site" == "gel" ]; then
 	echo $tag
 	danbooru-grabber -e gelbooru -dfn post:id_post:widthxpost:height -d "$tag"
 elif [ "$site" == "4chan" ]; then
+	# https://github.com/ypocat/4chan
 	shit=$(xtitle)
 	# remove titlestring
 	thread_title=${shit%-*}
 	# remove slashes
 	thread_title_dir=$(echo "$thread_title" | sed "s/\///g")
 	mkdir -p 4chan/"$thread_title_dir" && cd 4chan/"$thread_title_dir"
-	# https://github.com/ypocat/4chan
 	if [ "$3" == "watch" ]; then
 		4chan $url
 	else
 		4chan -s $url
 	fi
+elif [ "$site" == "comic" ]; then
+	# use dta with webcomic reader; downside- have to name folder (upside.. works with hundreds of sites)
+	# https://github.com/ameboide/webcomic_reader
+	# move away from content
+	xdotool mousemove 1316 708
+	# right click and open dta window
+	xdotool click 3 && xdotool mousemove_relative -- 0 -180 && sleep 0.1 && xdotool click 1
+	sleep 0.3
+	# highlight download dir for changing; then just hit enter
+	xdotool mousemove 282 505 && xdotool mousedown 1 && xdotool mousemove 475 505 && xdotool mouseup 1
 else
 	bspc rule -a termite -o floating=true center=true
 	termite -e "/bin/zsh -c 'xdo resize -w +300 && xdo move -x -150 && xdo resize -h +200 && xdo move -y -100 && ranger'" &
--- common/bin/pentadactyl/firebug_fake_mouse.sh
+++ common/bin/pentadactyl/firebug_fake_mouse.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # start video and wait for firebug to load it
-xdotool mousemove 519 326 && xdotool click 1 && sleep 3
+xdotool mousemove 519 326 click 1 && sleep 3
 
 # to prevent other menu from coming up, return to normal mode
 xdotool key Escape && sleep 0.2
 # open menu and copy link
-xdotool mousemove 284 713 && xdotool click 3
-sleep 0.4
-xdotool mousemove_relative -- 97 -262  && xdotool click 1
+xdotool mousemove 284 713 click 3
+sleep 0.5
+xdotool mousemove_relative -- 97 -262  click 1
 # stop video playing in broswser
-xdotool mousemove 519 326 && xdotool click 1 && sleep 0.2
+xdotool mousemove 519 326 click 1 && sleep 0.2
 xdotool key Escape
 # open link with mpv fullscreen
 clipboard_contents=$(xsel -b)
