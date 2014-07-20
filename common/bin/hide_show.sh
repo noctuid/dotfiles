619084092de903a0441178b774466e52831a25ee
angelic-sedition <gopv1c3@gmail.com>
2014-07-20 00:48:41 -0400

    replace old terminology dropdown (never use anyway)

--- common/bin/hide_show.sh
+++ common/bin/hide_show.sh
@@ -4,8 +4,16 @@
 if [ -n "$1" ]; then
 	if [ "$1" == "termite" ]; then
 		bspc rule -a termite -o floating=on
-		xdotool search --onlyvisible --classname $1 windowunmap || xdotool search --classname $1 windowmap || termite -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 16 && xdo resize -w +800 && tmux attach-session -dt dropdown || tmuxinator dropdown'" &
+		xdotool search --onlyvisible --classname $1 windowunmap || xdotool search --classname $1 windowmap || $1 -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 16 && xdo resize -w +800 && tmux attach-session -dt dropdown || tmuxinator dropdown'" &
+
+	elif [ "$1" == "Terminology" ]; then
+		bspc rule -a terminology -o floating=on
+		# inconsolata font; no visual bell; 256 colors
+		# window class is main for some bizzare reason
+		xdotool search --onlyvisible --classname main windowunmap || xdotool search --classname main windowmap || terminology -f=Inconsolata/12 -G=false -2 -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 14 && xdo resize -w +880 && tmux attach-session -dt pretty || tmux new-session -s pretty'"
+
 	else
+
 		xdotool search --onlyvisible --classname $1 windowunmap || xdotool search --classname $1 windowmap || exec $1 &
 	fi
 else
