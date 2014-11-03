4f3e8d0836692755a4341fb49b28176ddbbffaf5
angelic-sedition <nocturnalartifice@gmail.com>
2014-11-03 18:09:33 -0500

    add generic function for adding more terms
    also, awk makes more sense to me than a bunch of cuts

--- scripts/bin/hide_show.sh
+++ scripts/bin/hide_show.sh
@@ -2,19 +2,26 @@
 # https://bbs.archlinux.org/viewtopic.php?id=149444&p=54
 # http://angelic-sedition.github.io/blog/2014/07/19/make-any-terminal-emulator-a-quake-style-dropdown/
 
-win_name=$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS | cut -d '=' -f 2 | cut -d ',' -f 2 | sed 's/[\"\ ]//g')
+win_name=$(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | awk '{print $5}') WM_CLASS | awk '{ gsub(/"/, ""); print $4}')
+
+generic_toggle() {
+	(xdotool search --onlyvisible --classname $1 windowactivate && xdo hide -n $1) ||\
+	(bspc rule -a $1 -o floating=on && xdotool search --classname $1 windowmap) ||\
+	(bspc rule -a $1 -o floating=on && $1 -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 14 && xdo resize -w +800 && tmux attach-session -dt dropdown || tmuxinator dropdown'" &)
+}
 
 if [ -n "$1" ]; then
 	if [ "$1" == "termite" ]; then
-		(xdotool search --onlyvisible --classname $1 windowactivate && xdo hide -n $1) ||\
-		(bspc rule -a $1 -o floating=on && xdotool search --classname $1 windowmap) ||\
-		(bspc rule -a $1 -o floating=on && $1 -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 14 && xdo resize -w +800 && tmux attach-session -dt dropdown || tmuxinator dropdown'" &)
+		generic_toggle termite
+	elif [ "$1" == "xterm" ]; then
+		generic_toggle xterm
 	elif [ "$1" == "Terminology" ]; then
 		# inconsolata font; no visual bell; 256 colors
 		# window class is main for some bizzare reason
 		(xdotool search --onlyvisible --classname main windowactivate && xdo hide -n main) ||\
 		(bspc rule -a main -o floating=on && xdotool search --classname main windowmap) ||\
 		(bspc rule -a main -o floating=on && terminology -f=Inconsolata/12 -G=false -2 -e "/bin/zsh -c 'sleep 0.01 && xdo move -x 0 -y 14 && xdo resize -w +880 && tmux attach-session -dt pretty || tmux new-session -s pretty'" &)
+	# doing something different for urxvt (not floating); open on first desktop and switch to, then switch back to last window
 	elif [ "$1" == "urxvt" ]; then
 		if [ "$win_name" == "URxvt" ]; then
 			bspc window -f last
