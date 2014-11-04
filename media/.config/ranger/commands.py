223f345016911587d3f9ccefd4c0912ed9598e9f
angelic-sedition <nocturnalartifice@gmail.com>
2014-11-03 22:00:16 -0500

    add compress command

--- media/.config/ranger/commands.py
+++ media/.config/ranger/commands.py
@@ -77,6 +77,38 @@
 # ===================================================================
 
 from ranger.api.commands import *
+from ranger.core.loader import CommandLoader
+
+# https://wiki.archlinux.org/index.php/Ranger#Compression
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.env.cwd
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.env.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['compress ' + os.path.basename(self.fm.env.cwd.path) + ext for ext in extension]
 
 class alias(Command):
     """:alias <newcommand> <oldcommand>
