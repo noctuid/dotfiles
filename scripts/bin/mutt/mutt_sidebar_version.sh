09a61bfe651c357ff72efe6e17faf51fec49660a
angelic-sedition <nocturnalartifice@gmail.com>
2014-12-18 00:09:16 -0500

    move all sidebar configuration out of .muttrc

--- scripts/bin/mutt/mutt_sidebar_version.sh
+++ scripts/bin/mutt/mutt_sidebar_version.sh
@@ -2,18 +2,29 @@
 mutt_kz=$(mutt -v | grep mutt-kz)
 sidebar=$(mutt -v | grep sidebar)
 
-if [ $mutt_kz != "" ]; then
+if [ "$mutt_kz" != "" ]; then
 	# https://github.com/karelzak/mutt-kz/issues/82
 	# thanks to Dabsen
 	# mutt-kz only; colours delim
 	echo "color sidebar color223 color235"
-elif [ $sidebar != "" ];then
+elif [ "$sidebar" != "" ];then
 	# mutt-kz doesn't have these settings
 	# only show last part of path; this is how it is by default in mutt-kz
-	echo "sidebar_shortpath = yes"
+	echo "set sidebar_shortpath = yes"
 	# set sidebar_format = “%B%?F? [%F]?%* %?N?%N/?%4S”
 	# don't indent subfolders; mutt-kz indents
 	echo "set sidebar_folderindent = no"
+fi
+
+# for any version of mutt with the sidebar patch
+if [ "$sidebar" != "" ]; then
+	echo "macro index tb '<enter-command>toggle sidebar_visible<enter><refresh>'"
+	echo "macro pager tb '<enter-command>toggle sidebar_visible<enter><redraw-screen>'"
+	echo "set sidebar_delim = ' │'"
+	echo "set sidebar_visible = yes"
+	echo "set sidebar_width   = 34"
+	echo "color sidebar_new color221 color233"
+	# color sidebar_flagged
 fi
 
 # from man page:
