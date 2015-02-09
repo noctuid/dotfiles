f402241d48cdbeb7ab1129fdde8816276207252f
angelic-sedition <nocturnalartifice@gmail.com>
2015-02-09 17:37:01 -0500

    update mpv video play script correct positioning and take screenshots in current dir

--- scripts/bin/pentadactyl/firebug_fake_mouse.sh
+++ scripts/bin/pentadactyl/firebug_fake_mouse.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # start video and wait for firebug to load it
-xdotool mousemove 519 326 click 1 && sleep 3
-
+xdotool mousemove 519 326 click 1 && sleep 3 && \
 # to prevent other menu from coming up, return to normal mode
-xdotool key Escape && sleep 0.2
+xdotool key Escape && sleep 0.2 && \
 # open menu and copy link
-xdotool mousemove 284 713 click 3
-sleep 0.5
-xdotool mousemove_relative -- 97 -240 click 1
+xdotool mousemove 284 713 click 3 && \
+sleep 0.2 && \
+xdotool mousemove_relative -- 97 -215 && \
+sleep 0.2 && \
+xdotool click 1
 # stop video playing in broswser
 xdotool mousemove 519 326 click 1 && sleep 0.2
 xdotool key Escape
-# open link with mpv fullscreen
-clipboard_contents=$(xsel -b)
-mpv $clipboard_contents &
+# open link with mpv
+mpv --screenshot-template="./%tm.%td.%ty_%tH:%tM:%tS" "$(xsel -b)"
