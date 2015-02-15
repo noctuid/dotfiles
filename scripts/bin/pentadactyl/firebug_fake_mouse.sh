b491daa777904ccef12b80d3537d9adcc0f82d40
angelic-sedition <nocturnalartifice@gmail.com>
2015-02-15 00:04:02 -0500

    change screenshot format to year.month.day also, when playing videos from firefox in mpv, take screenshots in current directory

--- scripts/bin/pentadactyl/firebug_fake_mouse.sh
+++ scripts/bin/pentadactyl/firebug_fake_mouse.sh
@@ -6,11 +6,11 @@ xdotool key Escape && sleep 0.2 && \
 # open menu and copy link
 xdotool mousemove 284 713 click 3 && \
 sleep 0.2 && \
-xdotool mousemove_relative -- 97 -215 && \
+xdotool mousemove_relative -- 97 -230 && \
 sleep 0.2 && \
 xdotool click 1
 # stop video playing in broswser
 xdotool mousemove 519 326 click 1 && sleep 0.2
 xdotool key Escape
 # open link with mpv
-mpv --screenshot-template="./%tm.%td.%ty_%tH:%tM:%tS" "$(xsel -b)"
+mpv --screenshot-template="./%tY.%tm.%td_%tH:%tM:%tS" "$(xsel -b)"
--- scripts/bin/screenshot.sh
+++ scripts/bin/screenshot.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # get window title
 # remove punctuation (e.g.forward slashes) and replace whitespace with underscores
 title=$(xtitle | awk -F "-" '{ gsub(/[[:punct:]]/, "") gsub(/\s/, "_"); print $1}')
 
-if [ "$1" == "select" ]; then
+if [[ $1 == select ]]; then
 	# if use -s won't be able to use $W and $H in name
-	eval `slop`
-	maim -g $G ~/Move/Screenshots/$(date +%m.%d.%y_%H:%M:%S)_-_"$title"_"$W"x"$H".png
+	eval "$(slop)"
+	maim -g "$G" ~/Move/Screenshots/"$(date +%Y.%m.%d_%H:%M:%S)"_-_"$title"_"$W"x"$H".png
 else
-	maim ~/Move/Screenshots/$(date +%m.%d.%y_%H:%M:%S)_-_"$title".png
+	maim ~/Move/Screenshots/"$(date +%Y.%m.%d_%H:%M:%S)"_-_"$title".png
 fi
