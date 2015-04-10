22b528631a6ce4442dccbd6eb522ea8d9a947653
angelic-sedition <nocturnalartifice@gmail.com>
2015-04-09 23:27:19 -0400

    Add fixes for ranger as file saver script
    - Use temp files instead of writing to disk
    - Don't do anything if file was saved to /tmp (e.g. used "open with")
    Temporary:
    - Less stupid way of checking if called from private window
      (can't deal with on pentadactyl side currently due to autocmd pileup)
    - Prevent multiple windows being opened due to autocmd pileup

--- scripts/bin/ranger/ranger_browser_fm.sh
+++ scripts/bin/ranger/ranger_browser_fm.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-# see corresponding pentadactylrc section: ../../../browsing/.pentadactylrc:727
 # open ranger with downloaded file cut
+# see corresponding pentadactylrc section: ../../../browsing/.pentadactylrc:727
 
-window_title=$(xtitle)
-# remove everything until last hyphen; results in titlestring (mine is Pentadactyl; (Private Browsing) added if private win)
-browser_type=${window_title##*-}
-
-mkdir -p ~/bin/ranger
+mkdir -p /tmp/ranger_fm
 # toggle script
 if [[ $1 == "toggle" ]]; then
-	if [[ -f ~/bin/ranger/off.txt ]]; then
-		rm ~/bin/ranger/off.txt
+	if [[ -f /tmp/ranger_fm/off ]]; then
+		rm /tmp/ranger_fm/off
 	else
-		touch ~/bin/ranger/off.txt
+		touch /tmp/ranger_fm/off
 	fi
 else
-	if [[ ! -f ~/bin/ranger/off.txt ]]; then
-		if [[ $browser_type == " Pentadactyl" ]]; then
+	# conditionally executing the autocmd on the pentadactyl side is useless because of autocmd pileup
+	# until this is fixed; I'm using this instead of manually adding the DownloadPost autocmd
+	is_private=$(xtitle | grep "Private Browsing")
+	if [[ ! -f /tmp/ranger_fm/off ]] && [[ -z $is_private ]]; then
+		filename="$1"
+		# this is also to deal with autocmd pileup
+		last_filename="$(< /tmp/ranger_fm/last_filename)"
+		echo "$filename" > /tmp/ranger_fm/last_filename
+		if [[ ! $filename =~ ^/tmp.* ]] && [[ ! $last_filename == "$filename" ]]; then
 			# need to get rid of apostrophies and deal with spaces
-			no_ap=${1//\'/}
-			mv "$1" "$no_ap"
+			no_ap=${filename//\'/}
+			mv "$filename" "$no_ap"
 			escaped=$(printf '%q' "$no_ap")
 			bspc rule -a termite -o floating=true center=true
-			termite -e "/bin/bash -c 'xdo resize -w +300 && xdo move -x -150 && xdo resize -h +200 && xdo move -y -100 && ranger --selectfile=""$escaped"" --cmd=cut'" &
-		# else
-		# otherwise, a private window; do nothing
+			termite -e "/bin/bash -c 'xdo resize -w +300 && xdo move -x -150 && xdo resize -h +200 && \
+				xdo move -y -100 && ranger --selectfile=""$escaped"" --cmd=cut'" &
 		fi
 	fi
 fi
