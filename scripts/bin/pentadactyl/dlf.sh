31e6bc0e9e88747a7fdbc4587eeed80b1d13962c
angelic-sedition <noct@openmailbox.org>
2015-06-02 19:36:11 -0400

    Update download script and add 8chan support
    - Use double brackets and quote variables (prevent shellcheck
    complaining)
    - Add 8chan pentadactyl group
    - Stop using javascript 4chan thread downloader and just use curl and
    aria2c
    - Get rid of functionality to uselessly open ranger if given no args

--- scripts/bin/pentadactyl/dlf.sh
+++ scripts/bin/pentadactyl/dlf.sh
@@ -1,42 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 # happy fun download time
-cd ~/database/move
 
 site=$1
 url=$2
 
-# if [ "$site" == "fakku" ]; then
+takedir() {
+	mkdir -p "$1"
+	cd "$1"
+}
+
+takedir ~/database/move
+
+# if [[ $site == fakku ]]; then
 	# mkdir -p fakku && cd fakku
 	# fakku.py (renamed without the .py or change below) in path: https://github.com/darkfeline/fakku_downloader
 	# it broke; using comic
 	# fakku $url &
-if [ "$site" == "gel" ]; then
+if [[ $site == gel ]]; then
 	# in aur https://code.google.com/p/danbooru-v7sh-grabber/
-	mkdir -p gelbooru && cd gelbooru
+	takedir gelbooru
 	tag=${url##*=}
-	echo $tag
 	danbooru-grabber -e gelbooru -dfn post:id_post:widthxpost:height -d "$tag"
-elif [ "$site" == "4chan" ]; then
-	# https://github.com/ypocat/4chan
-	shit=$(xtitle)
-	# remove titlestring
-	thread_title=${shit%-*}
-	# remove slashes
-	thread_title_dir=$(echo "$thread_title" | sed "s/\///g")
-	mkdir -p 4chan/"$thread_title_dir" && cd 4chan/"$thread_title_dir"
-	if [ "$3" == "watch" ]; then
-		4chan $url
+elif [[ $site == *chan ]]; then
+	thread=$(curl "$url")
+	if [[ $site == 4chan ]]; then 
+		# downloads images, gifs, and webms in thread
+		# alternatively https://github.com/youurayy/4chan for features like watching
+		title=$(echo "$thread" | grep -o '<meta name="description" content="[^"]*' | sed -e "s/^.*content=\"//" -e "s/ -.*//")
+		takedir 4chan/"${title//[\/ ]/_}"
+		echo "$thread" | grep -o '<a class="fileThumb" href="//i.4cdn.org/[^"]*' | sed "s%^.*href=\"%https:%" > tmp_urls
 	else
-		4chan -s $url
+		title=$(echo "$thread" | grep -o '<title>[^<]*' | sed 's/^<title>//')
+		takedir 8chan/"${title//[\/ ]/_}"
+		echo "$thread" | grep -o '<a href="https://media.8ch.net/tech/src/[^"]*' | sed "s/<a href=\"//" | uniq > tmp_urls
 	fi
-elif [ "$site" == "comic" ]; then
+	aria2c -i tmp_urls
+	rm tmp_urls
+elif [[ $site == comic ]]; then
 	# use dta with webcomic reader; downside- have to name folder (upside.. works with hundreds of sites)
 	# https://github.com/ameboide/webcomic_reader
 	# use emenu to open dta
 	sleep 0.3
-	# highlight download dir for changing; then just hit enter
+	# highlight download dir for changing; then just hit enter; this is crap.. but it works
 	xdotool mousemove 275 522 && xdotool mousedown 1 && xdotool mousemove 551 530 && xdotool mouseup 1
-else
-	bspc rule -a termite -o floating=true center=true
-	termite -e "/bin/zsh -c 'xdo resize -w +300 && xdo move -x -150 && xdo resize -h +200 && xdo move -y -100 && ranger'" &
 fi
