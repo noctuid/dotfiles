3e2825c8a605a3b252603b73e68bf20ee540babe
angelic-sedition <noct@openmailbox.org>
2015-06-02 19:52:27 -0400

    Delete stupid music scripts

--- scripts/bin/music/visualizer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Since vimus doesn't support visualization, I just bind a key to open ncmpcpp in a split and do the visualizatoin
-
-# open split
-tmux split-window -h
-# get vimus back
-tmux select-pane -l && xdotool key Escape && xdotool key control+d && tmux select-pane -l
-# open visualizer
-tmux send-keys "ncmpcpp" && tmux send-keys "Enter" && tmux send-keys "8"
