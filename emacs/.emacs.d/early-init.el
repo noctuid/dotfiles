19bc0b2b1572093b1cc73d9647dca5daf3f0dd2e
Fox Kiester <noctuid@pir-hana.cafe>
2022-06-29 22:35:03 -0400

    Font - Use Delugia Code (Cascadia/Caskaydia Cove)
    - Use Delugia in Emacs, kitty, and polybar
    - Check for font existence before adding font to default-frame-alist in
      early-init.el (otherwise Emacs will fail to start); store known fonts to skip check
    - Add ligature.el
    - Update noct-set-font to allow selecting font from favorites

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -10,12 +10,13 @@
 ;; can check later that `package--initialized' is nil
 
 ;; ** Disable Tool Bar, Menu Bar, and Scroll Bar
-;; doing this here reduces init time by ~0.2 seconds
+;; doing this here reduces init time by ~0.2 seconds for me
 ;; disabling `tool-bar-mode' in normal init file takes ~0.1s
 ;; https://github.com/raxod502/radian/issues/180
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(push '(horizontal-scroll-bars) default-frame-alist)
 (let ((gap (or (getenv "WM_GAP") "15")))
   (push (cons 'internal-border-width (string-to-number gap)) default-frame-alist))
 
@@ -27,11 +28,56 @@
 ;; I haven't actually noticed any difference for fonts I've tried
 (setq frame-inhibit-implied-resize t)
 
-;; TODO `find-font' doesn't work in early init, and Emacs fails if it can't find
-;; the font
-;; (let ((font "Office Code Pro D"))
-;;   (when (or t (find-font (font-spec :name font)))
-;;     (push `(font .  ,(concat font "-10")) default-frame-alist)))
+(defconst noct-preferred-fonts (list
+                                ;; Delugia is basically a better CaskaydiaCove
+                                ;; (mainly it has italics and CaskaydiaCove Nerd
+                                ;; Font still doesn't)
+                                "Delugia-10"
+                                "CaskaydiaCove Nerd Font-10"
+                                "Cascadia Code-10"
+                                "Office Code Pro D-10")
+  "Fonts to try to use as the default if they exist(in order of priority).")
+
+(require 'cl-lib)
+
+(defun noct-maybe-set-default-font (&optional frame)
+  "Before creating the first frame, set the default font.
+The point of this is to set the font using `default-frame-alist' rather than
+`set-face-attribute' or `set-frame-font' as the former is faster.  Adding this
+to `after-make-frame-functions' is necessary because `find-font' does not work
+immediately in early-init.el.  However, this will not work for the first frame
+with a daemon Emacs, so this function will store found fonts, so that we can
+check for them wit h`noct-known-font' in the early-init.el.  Ideally there is
+some better way to do this.
+
+If the font is already set in `default-frame-alist', this will still try to look
+for fonts that appear before it in `noct-preferred-fonts' (if there are any) so
+that preferred fonts are still discovered and marked as known/existing."
+  (let ((current-font (assq 'font default-frame-alist)))
+    (cl-dolist (font noct-preferred-fonts)
+      (when (equal font current-font)
+        (cl-return))
+      (when (find-font (font-spec :name font) frame)
+        (push (cons 'font font) default-frame-alist)
+        (with-temp-buffer
+          (write-file (expand-file-name font user-emacs-directory)))
+        (cl-return)))))
+
+(defun noct-known-font ()
+  "Return a font from `noct-preferred-fonts' that has been previously found.
+If no fonts have been found, return nil."
+  (cl-dolist (font noct-preferred-fonts)
+    (when (file-exists-p (expand-file-name font user-emacs-directory))
+      (cl-return font))))
+
+(let ((known-font (noct-known-font)))
+  (when known-font
+    (push (cons 'font known-font) default-frame-alist))
+  (unless (and known-font
+               ;; still check if #1 preferred font exists after init
+               (equal known-font (car noct-preferred-fonts)))
+    ;; this is too late when using the server
+    (add-hook 'after-make-frame-functions #'noct-maybe-set-default-font)))
 
 ;; ** Don't Load Site Startup File or Default Init File
 ;; not loading default.el actually does have an impact (sometimes? it seems like
